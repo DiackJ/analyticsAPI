2c964a125c0abd5e53d4bf7cac4c3bd224f2d559
Jessica <diackjessica5@gmail.com>
2025-06-02 18:42:42 -0400

    initial commit

---
+++ portfolio/analyticsApi/api/src/test/java/com/analytics/api/AnalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analytics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
